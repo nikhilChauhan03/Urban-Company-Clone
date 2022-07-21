dce923f51094fb64d0207450bca2f0c09fe09995
NLucifer03 <nikhilthakur3300@gmail.com>
2022-07-21 23:09:22 +0530

    adding summary page

---
+++ script'/summary.js
@@ -0,0 +1,7 @@
+document.querySelector(".nikhil_add_address").addEventListener("click",addressPop)
+
+function addressPop()
+{
+    let btn = document.querySelector(".nikhil_address_container")
+    console.log("inside function")
+}
