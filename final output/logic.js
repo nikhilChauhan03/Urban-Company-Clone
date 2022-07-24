ab3e1e0fee5089c4bacd4f9a11023617f36990ee
kunal <kdeotale46@gmail.com>
2022-07-24 11:27:44 +0530

    add home page

---
+++ final output/logic.js
@@ -0,0 +1,38 @@
+function hair(){
+    window.location.href='https://www.youtube.com/'
+}
+
+function salon(){
+    window.location.href
+}
+
+function women(){
+    window.location.href
+}
+
+function men(){
+    window.location.href
+}
+
+function electric(){
+    window.location.href
+}
+function ac(){
+    window.location.href
+}
+
+function cleaning(){
+    window.location.href
+}
+
+function personal(){
+    window.location.href
+}
+
+function plumber(){
+    window.location.href
+}
+
+function salonWomen(){
+    window.location.href
+}
